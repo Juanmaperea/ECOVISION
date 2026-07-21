c32d07edbcc4588c4936b6320bdaaa24b9aef80c
Hassen David Ortiz <hassen.ortiz@correounivalle.edu.co>
2026-07-21 14:40:52 -0500

    Update router.py

--- backend/app/modules/health/router.py
+++ backend/app/modules/health/router.py
@@ -3,17 +3,25 @@ from fastapi import APIRouter
 from app.schemas.health import HealthResponse
 from app.modules.health.service import HealthService
 
+
 router = APIRouter(
     prefix="/health",
-    tags=["Health"]
+    tags=["Health"],
 )
 
 
 @router.get(
     "",
     response_model=HealthResponse,
-    summary="Verifica el estado de la API"
+    summary="Verifica el estado de la API",
 )
 def health():
-
     return HealthService.get_status()
+
+
+@router.get(
+    "/detailed",
+    summary="Verifica el estado detallado de los servicios",
+)
+def detailed_health():
+    return HealthService.get_detailed_status()
